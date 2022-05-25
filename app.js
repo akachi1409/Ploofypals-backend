a851ae2c37ae662bf2226c1e4c07423bb367d782
trusteddev1409 <trusteddev1325@gmail.com>
2022-05-26 08:50:58 +0900

    change port 5000 to 80

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const fileUpload = require("express-fileupload");
 
 const app = express();
-const PORT = 5000;
+const PORT = 80;
 
 // default options
 app.use(fileUpload());
