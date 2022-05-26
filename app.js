ce3cdd92dfb9687019a98999de46cafa8881d39f
trusteddev1409 <trusteddev1325@gmail.com>
2022-05-26 13:37:51 +0900

    remove app server

--- app.js
+++ app.js
@@ -78,14 +78,14 @@ app.post("/upload", function (req, res) {
   res.send("File uploaded!")
 });
 
-app.listen(PORT, (error) => {
-  if (!error) {
-    console.log(
-      "Server is Successfully Running, and App is listening on port " + PORT
-    );
+// app.listen(PORT, (error) => {
+//   if (!error) {
+//     console.log(
+//       "Server is Successfully Running, and App is listening on port " + PORT
+//     );
     
-  } else console.log("Error occurred, server can't start", error);
-});
+//   } else console.log("Error occurred, server can't start", error);
+// });
 
 https.createServer(
   {
